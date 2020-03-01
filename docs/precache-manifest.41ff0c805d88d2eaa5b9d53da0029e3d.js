a578f78095ce598259a73183ed8b33252ad9ce17
Roman Vozbrannyi <romanvozbrannyi@gmail.com>
2020-03-02 00:08:54 +0200

    try gh-pages

---
+++ docs/precache-manifest.41ff0c805d88d2eaa5b9d53da0029e3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3831c20c0f0d6145b7d4d9433d118376",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1bc9080d03f1f129b8b2",
+    "url": "/static/css/2.28b0024a.chunk.css"
+  },
+  {
+    "revision": "21b593cf9edcc0b01c58",
+    "url": "/static/css/main.8c1cc53c.chunk.css"
+  },
+  {
+    "revision": "1bc9080d03f1f129b8b2",
+    "url": "/static/js/2.40ed70e5.chunk.js"
+  },
+  {
+    "revision": "954058033d03348f317e547ae00e0a0e",
+    "url": "/static/js/2.40ed70e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21b593cf9edcc0b01c58",
+    "url": "/static/js/main.d9002437.chunk.js"
+  },
+  {
+    "revision": "2a734556935bf03ccb14",
+    "url": "/static/js/runtime-main.0578a964.js"
+  }
+]);
