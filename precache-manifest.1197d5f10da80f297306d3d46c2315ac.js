eb9e2c91abe37e10dddf1a045cf46389eef51b2c
Roman Vozbrannyi <romanvozbrannyi@gmail.com>
2020-03-02 00:19:00 +0200

    Updates

---
+++ precache-manifest.1197d5f10da80f297306d3d46c2315ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93c328449fcc638ff3bbd9d12aec8944",
+    "url": "/react-table/index.html"
+  },
+  {
+    "revision": "1bc9080d03f1f129b8b2",
+    "url": "/react-table/static/css/2.28b0024a.chunk.css"
+  },
+  {
+    "revision": "21b593cf9edcc0b01c58",
+    "url": "/react-table/static/css/main.8c1cc53c.chunk.css"
+  },
+  {
+    "revision": "1bc9080d03f1f129b8b2",
+    "url": "/react-table/static/js/2.40ed70e5.chunk.js"
+  },
+  {
+    "revision": "954058033d03348f317e547ae00e0a0e",
+    "url": "/react-table/static/js/2.40ed70e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21b593cf9edcc0b01c58",
+    "url": "/react-table/static/js/main.d9002437.chunk.js"
+  },
+  {
+    "revision": "203078afe9b033ce9f1b",
+    "url": "/react-table/static/js/runtime-main.060b5625.js"
+  }
+]);
